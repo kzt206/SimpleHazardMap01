6ceb0cd8452fcd1ecd200028d49d572feb1b366c
kzt206 <kzt206@gmail.com>
2021-07-28 15:53:00 +0900

    Add basic stamp function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+//キャンバスの設定
 const canvasMap = document.querySelector("#canvasMap");
 const ctxMap = canvasMap.getContext("2d");
 const canvasPaint = document.querySelector("#canvasPaint");
@@ -9,9 +12,7 @@ canvasMap.height = 600;
 canvasPaint.width = 800;
 canvasPaint.height = 600;
 
-// ctx01.fillRect(10,10,30,20);
-// ctx02.fillRect(20,30,50,100);
-
+//マップ画像の読み込み
 const selectMapFile = document.getElementById("selectMapFile");
 selectMapFile.addEventListener("change",function(evt){
 
@@ -37,3 +38,46 @@ selectMapFile.addEventListener("change",function(evt){
     }
 
 },false);
+
+
+//スタンプの設置
+//スタンプ画像の設定
+const charaHome = new Image();
+charaHome.src = "home.png"
+const charaSchool = new Image();
+charaSchool.src = "school.png"
+const charaEvacuation = new Image();
+charaEvacuation.src = "evacuation.png"
+
+//スタンプを選択
+let penStatus = "pen";
+const homeButton = document.getElementById("homeButton");
+homeButton.addEventListener("click",()=>{
+    penStatus = "home";
+    console.log("Home Stamp selected");
+})
+const schoolButton = document.getElementById("schoolButton");
+schoolButton.addEventListener("click",()=>{
+    penStatus = "school";
+    console.log("School Stamp selected");
+})
+const evacuationButton = document.getElementById("evacuationButton");
+evacuationButton.addEventListener("click",()=>{
+    penStatus = "evacuation";
+    console.log("Evacuation Stamp selected");
+})
+
+this.canvasPaint.addEventListener("click",(e) => {
+    let x = e.offsetX-25;
+    let y = e.offsetY-25;
+
+    console.log("x:",x,"y:",y);
+    console.log("Penstatus;",penStatus);
+      if(penStatus == "home") {
+        ctxPaint.drawImage(charaHome,x,y,20,20);
+      } else if(penStatus == "school"){
+        ctxPaint.drawImage(charaSchool,x,y);
+      } else if(penStatus == "evacuation"){
+        ctxPaint.drawImage(charaEvacuation,x,y);
+      }
+})
