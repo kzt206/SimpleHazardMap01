eb056e0a06571f6707422b5ae5ee94649a64285f
kzt206 <kzt206@gmail.com>
2021-07-29 10:43:27 +0900

    Add camera stamps function

--- script.js
+++ script.js
@@ -48,6 +48,17 @@ const charaSchool = new Image();
 charaSchool.src = "school.png"
 const charaEvacuation = new Image();
 charaEvacuation.src = "evacuation.png"
+const charaCamera1 = new Image();
+charaCamera1.src = "camera1.png"
+const charaCamera2 = new Image();
+charaCamera2.src = "camera2.png"
+const charaCamera3 = new Image();
+charaCamera3.src = "camera3.png"
+const charaCamera4 = new Image();
+charaCamera4.src = "camera4.png"
+const charaCamera5 = new Image();
+charaCamera5.src = "camera5.png"
+
 
 //スタンプを選択
 let penStatus = "pen";
@@ -66,6 +77,33 @@ evacuationButton.addEventListener("click",()=>{
     penStatus = "evacuation";
     console.log("Evacuation Stamp selected");
 })
+const camera1Button = document.getElementById("camera1Button");
+camera1Button.addEventListener("click",()=>{
+    penStatus = "camera1";
+    console.log("camera1 stamp selected");
+})
+const camera2Button = document.getElementById("camera2Button");
+camera2Button.addEventListener("click",()=>{
+    penStatus = "camera2";
+    console.log("camera2 stamp selected");
+})
+const camera3Button = document.getElementById("camera3Button");
+camera3Button.addEventListener("click",()=>{
+    penStatus = "camera3";
+    console.log("camera3 stamp selected");
+})
+const camera4Button = document.getElementById("camera4Button");
+camera4Button.addEventListener("click",()=>{
+    penStatus = "camera4";
+    console.log("camera4 stamp selected");
+})
+const camera5Button = document.getElementById("camera5Button");
+camera5Button.addEventListener("click",()=>{
+    penStatus = "camera5";
+    console.log("camera5 stamp selected");
+})
+
+
 //消しゴムの設定
 const eraserButton = document.getElementById("eraserButton");
 eraserButton.addEventListener("click",() => {
@@ -87,6 +125,16 @@ this.canvasPaint.addEventListener("click",(e) => {
         ctxPaint.drawImage(charaSchool,x,y);
       } else if(penStatus == "evacuation"){
         ctxPaint.drawImage(charaEvacuation,x,y);
+      } else if(penStatus == "camera1"){
+        ctxPaint.drawImage(charaCamera1,x,y);
+      } else if(penStatus == "camera2"){
+        ctxPaint.drawImage(charaCamera2,x,y);
+      } else if(penStatus == "camera3"){
+        ctxPaint.drawImage(charaCamera3,x,y);
+      } else if(penStatus == "camera4"){
+        ctxPaint.drawImage(charaCamera4,x,y);
+      } else if(penStatus == "camera5"){
+        ctxPaint.drawImage(charaCamera5,x,y);
       } else if(penStatus == "eraser"){
         ctxPaint.clearRect(e.offsetX-10,e.offsetY-10,20,20);
       }
